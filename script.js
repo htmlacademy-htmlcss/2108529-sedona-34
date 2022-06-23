215e44e2c4af13a0342ea3ac413e186803348243
Олег Шевченко <shevchenko.code@yandex.ru>
2022-06-23 10:49:29 +0500

    Добавляет модальное окно и все оставшиеся стили + изменяет структуру папки images

---
+++ script.js
@@ -0,0 +1,14 @@
+// opening and closing modal window
+let modal = document.querySelector(".modal-container");
+let hotelSearchButton = document.querySelector(".hotel-search-button");
+let modalCloseButton = document.querySelector(".modal-close-button");
+
+hotelSearchButton.onclick = function () {
+  modal.classList.toggle("modal-closed")
+}
+
+modalCloseButton.onclick = function () {
+  modal.classList.toggle("modal-closed");
+}
+
+// warning message output
